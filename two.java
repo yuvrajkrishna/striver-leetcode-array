e08e54d39ef45471e88b62bf229eed83482f0f76
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-13 10:55:07 +0530

    second largest

--- two.java
+++ two.java
@@ -3,6 +3,9 @@ public class two {
         int nums[] = {1, 2, 4, 7, 7, 5};
         int max = Integer.MIN_VALUE;
         int secmax = Integer.MIN_VALUE;
+        if(nums == null || nums.length < 2){
+            throw new IllegalArgumentException("array doesn't contains required elements");
+        }
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] > max){
                 secmax = max;
@@ -11,6 +14,9 @@ public class two {
             else if (nums[i] > secmax && nums[i] != max){
                 secmax = nums[i];
             } 
+        }
+        if(secmax == Integer.MIN_VALUE){
+            throw new IllegalArgumentException("second max doesn't exist");
         }
         System.out.println(secmax);
     }
